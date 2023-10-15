f9eb9d8d0c0ceb0fa769b6cbb639126348d3b6ac
karthick.shiva <karthick.shiva@zohocorp.com>
2023-10-15 23:33:48 +0530

    Script modified.

--- cron_job.sh
+++ cron_job.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# Define the project path
+PROJECT_PATH="/home/karthickshiva1709_gmail_com/projects/ticketnew"
+
+# Change the current working directory to the project path
+cd "$PROJECT_PATH" || { echo "Error: Could not change directory to $PROJECT_PATH"; exit 1; }
+
 # Define the path to your Python script
-SCRIPT_PATH="/home/karthickshiva1709_gmail_com/projects/ticketnew/main.py"
+SCRIPT_PATH="./main.py"  # Relative path within the project directory
 
 # Define the cron schedule (every 2 minutes)
 CRON_SCHEDULE="*/2 * * * *"
 
+# Define the path to the output log file (relative to the project directory)
+LOG_FILE="./cron_output.log"
+
 # Check if the cron job already exists
-if crontab -l | grep -q "$SCRIPT_PATH"; then
+if crontab -l | grep -q "$PROJECT_PATH/$SCRIPT_PATH"; then
     echo "Cron job for the script already exists."
 else
-    # Add the cron job
-    (crontab -l; echo "$CRON_SCHEDULE /usr/bin/python3 $SCRIPT_PATH") | crontab -
-    echo "Cron job added for the script."
+    # Add the cron job with output redirection to the log file
+    (crontab -l; echo "$CRON_SCHEDULE /usr/bin/python3 $PROJECT_PATH/$SCRIPT_PATH >> $PROJECT_PATH/$LOG_FILE 2>&1") | crontab -
+    echo "Cron job added for the script with output to $PROJECT_PATH/$LOG_FILE."
 fi
